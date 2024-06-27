44e2708a091e484cd8c3bf5542bf0b5c54e19c0c
hokhoa05 <khoaholtv0304@gmail.com>
2024-06-27 14:16:56 +0700

    Comment in Structures.h

--- Group-Project-Dictionary/src/Data-Structures/Structures.h
+++ Group-Project-Dictionary/src/Data-Structures/Structures.h
@@ -3,16 +3,25 @@
 struct Word;
 struct Definition;
 struct Word {
-	bool isFavorite;
-	std::string data;
-	std::vector<Definition*> defs;
-	Word(const std::string &str);
-	~Word();
+	bool isFavorite; //Check if Word is in favorite list
+	std::string data; //The word
+	std::vector<Definition*> defs; 
+	/*
+		Definitions of the word, a word can be linked to several of definitions.
+		Using pointer.
+	*/
+	
+	Word(const std::string &str); // Constructor
+	~Word(); // Destructor
 };
 
 struct Definition {
-	std::string data;
+	std::string data; //The definition
 	Word *word;
-	Definition(const std::string& str);
-	~Definition();
+	/*
+		A definition is linked to a word.
+		Using pointer.
+	*/
+	Definition(const std::string& str); // Constructor
+	~Definition(); // Destructor
 };
